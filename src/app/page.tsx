b0bf42e0164bfc3388ae6112bddd9fabbfa5acf1
Vito Medlej <vito.medlej@gmail.com>
2024-02-19 15:51:40 +0200

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,8 @@ const fetchData = async () => {
   try{
 
   
-  const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ next:{revalidate:10} })
+  // const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{ next:{revalidate:10} })
+  const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`,{cache:'no-store', next: { revalidate: 0 }})
   // const req = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/get-data`)
   let res = req &&  await req.json();
   if (res) {
