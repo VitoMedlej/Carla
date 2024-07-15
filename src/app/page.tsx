aaf5b34945ed39b13acc66cc474ddf7962292917
Vito Medlej <vito.medlej@gmail.com>
2024-07-15 16:11:01 +0300

    static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,36 @@ import PreLoader from "@/Components/PreLoader"
 
 
 
-const fetchDataAndSetImgs = async () => {
-  try {
+// const fetchDataAndSetImgs = async () => {
+//   try {
 
   
-    const response = await fetch('https://api.jsonbin.io/v3/b/65d0a0e41f5677401f30945c', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Master-Key': '$2a$10$oO7JF9H9fEhY6itLndL/kOBlo6DoOM0X6vXK8SnJxF4GOM7K4XWo2'
-      },
-    // next:{revalidate:1000}
-  },
-  )
-  if (!response?.ok) {
-    throw new Error(`Failed to fetch data. Status: ${response?.status}`);
-  }
+//     const response = await fetch('https://api.jsonbin.io/v3/b/65d0a0e41f5677401f30945c', {
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-Master-Key': '$2a$10$oO7JF9H9fEhY6itLndL/kOBlo6DoOM0X6vXK8SnJxF4GOM7K4XWo2'
+//       },
+//     // next:{revalidate:1000}
+//   },
+//   )
+//   if (!response?.ok) {
+//     throw new Error(`Failed to fetch data. Status: ${response?.status}`);
+//   }
 
-  const data = await response.json();
-  console.log('data: ', data);
-  if (data?.record) {
+//   const data = await response.json();
+//   console.log('data: ', data);
+//   if (data?.record) {
 
-    return data?.record; // Assuming 'record' is the property containing the images in the response
-  }
-  return null
-}
-catch(e){
-  console.log('e: ', e);
-  return null
-}
-}
+//     return data?.record; // Assuming 'record' is the property containing the images in the response
+//   }
+//   return null
+// }
+// catch(e){
+//   console.log('e: ', e);
+//   return null
+// }
+// }
 const fetchData = async () => {
   try{
 
@@ -98,8 +98,8 @@ try {
       // console.log('res: ', res);
       // const reqImages = await fetch(`https://getpantry.cloud/apiv1/pantry/732d3c8c-d53a-4c4c-830c-fe9b7e021958/basket/Images`,{  cache:'no-store', next: { revalidate: 400 } })
       // let resImages : any = await  reqImages.json();
-      let resImages : any =await fetchDataAndSetImgs()
-      // let resImages : any = null
+      // let resImages : any =await fetchDataAndSetImgs()
+      let resImages : any = null
       // let res = {data:{featuredProducts:null}}
       
       return (
